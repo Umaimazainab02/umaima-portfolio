25efd509d0b187c04544dbf2d801a59a3a9cf6a1
Umaima Zainab <umaimazainab429@gmail.com>
2025-09-17 20:33:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+// Links pe click hone ke baad menu close karne ke liye
+document.querySelectorAll(".nav-link").forEach(n =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
